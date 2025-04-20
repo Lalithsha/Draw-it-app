c3eb0d8c124a78ea4172499d0038cf1c3589e89f
Lalith Sharma <lalithsharma989@gmail.com>
2025-04-20 13:46:24 +0530

    Added drawing multiple rectangles & structured draw logic

--- apps/excelidraw-frontend/draw/index.ts
+++ apps/excelidraw-frontend/draw/index.ts
@@ -1,7 +1,23 @@
+type Shape = {
+    type:"rect";
+    x:number;
+    y:number;
+    width:number;
+    height:number;
+} | {
+    type:"circle";
+    centerX:number;
+    centerY:number;
+    radius:number;
+}
+
+
 export default function initDraw(canvas: HTMLCanvasElement){
   
     const ctx = canvas.getContext("2d");
 
+    let existingShape: Shape[] =[];
+    
     if (!ctx) {
         return;
     }
@@ -23,17 +39,45 @@ export default function initDraw(canvas: HTMLCanvasElement){
         clicked = false;
         console.log("Mouse up", e.clientX);
         console.log("Mouse up", e.clientY);
+        const width = e.clientX-startX;
+        const height = e.clientY-startY;
+        existingShape.push({
+            type:"rect",
+            x:startX,
+            y:startY,
+            width,
+            height
+        })
+
+        
     });
 
     canvas.addEventListener("mousemove", (e) => {
         if (clicked) {
         const width = e.clientX - startX;
         const height = e.clientY - startY;
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.fillStyle = "rgba(0, 0, 0)";
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        clearCanvas(existingShape, canvas);
         ctx.strokeStyle = "rgba(255, 255, 255)";
         ctx.strokeRect(startX, startY, width, height);
         }
     });
+}
+
+function clearCanvas(existingShape:Shape[], canvas: HTMLCanvasElement){
+
+    const ctx = canvas.getContext("2d");
+    if(!ctx){
+        return null;
+    }
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "rgba(0, 0, 0)";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    existingShape.map((shape)=>{
+        if(shape.type==='rect'){
+            ctx.strokeStyle = "rgba(255, 255, 255)";
+            ctx.strokeRect(shape.x, shape.y, shape.width, shape.height);
+        }
+    })
+    
 }
--- apps/excelidraw-frontend/src/app/canvas/[roomId]/page.tsx
+++ apps/excelidraw-frontend/src/app/canvas/[roomId]/page.tsx
@@ -9,16 +9,30 @@ export default function Canvas() {
     if (canvasRef.current) {
       initDraw(canvasRef.current);
     }
+
+    // return () => {
+    //   canvas.removeEventListener("mousedown", handleMouseDown);
+    //   canvas.removeEventListener("mouseup", handleMouseUp);
+    //   canvas.removeEventListener("mousemove", handleMouseMove);
+    // };
   }, [canvasRef]);
 
   return (
     <div>
       <canvas
         ref={canvasRef}
-        width={2000}
-        height={500}
+        width={1534}
+        height={693}
         style={{ border: "1px solid black" }}
       ></canvas>
+      <div className="absolute bottom-0 right-0">
+        <button className="bg-white text-black m-2 p-2 rounded-md">
+          Rectangle
+        </button>
+        <button className="bg-white text-black m-2 p-2 rounded-md">
+          Circle
+        </button>
+      </div>
     </div>
   );
 }
