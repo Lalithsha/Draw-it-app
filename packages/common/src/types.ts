14f2007b3f34ba5a96dea94e3df5e873995f21fc
Lalith Sharma <lalithsharma989@gmail.com>
2025-04-30 00:17:52 +0530

    Fixed zod schema for sign up - create user schema

--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -1,7 +1,7 @@
 import {z} from "zod";
 
 export const CreateUserSchema = z.object({
-    username: z.string().min(3,{message:"Username must be greater than 3 characters"}).max(20,{message:"Username must be less than 20 characters"}),
+    username: z.string().min(3,{message:"Username must be greater than 3 characters"}).max(50,{message:"Username must be less than 50 characters"}),
     password: z.string().min(6,{message:"Password must be greater than 3 characters"}).max(20,{message:"Password cannot be greater than 20 characters"}),
     name: z.string().min(3).max(20)
 })
